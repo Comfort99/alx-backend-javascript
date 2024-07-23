4873068b3e5a869a24a3ae34d248892bcbd1bb69
Comfort99 <shingangecomfort@gmail.com>
2024-07-23 20:34:50 +0200

    ES6 basics

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    const newArray = []
+    for (let idx of array) {
+      newArray.push(appendString + idx);
+    }
+  
+    return newArray;
+  }
