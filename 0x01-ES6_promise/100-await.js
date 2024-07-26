20140920d5a731da11fa162b552efc57f63f07a7
Comfort99 <shingangecomfort@gmail.com>
2024-07-27 00:39:56 +0200

    await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const promises = [
-    uploadPhoto(),
-    createUser(),
-  ];
-
-  const results = await Promise.allSettled(promises);
-
-  const response = {
-    photo: results[0].status === 'fulfilled' ? results[0].value : null,
-    user: results[1].status === 'fulfilled' ? results[1].value : null,
-  };
-
-  return response;
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
 }
